eaa31422cf74e9a346c4f9e0a5b767ec4dc49934
Genil-son <143018894+Genil-son@users.noreply.github.com>
2023-08-23 21:55:22 +0100

    Meu site

---
+++ main.js
@@ -0,0 +1,7 @@
+var menu = document.querySelector('nav ul');
+var menuBar = document.querySelector('nav .menu-icon');
+var iconMenu = document.querySelector('nav .menu-icon svg');
+
+menuBar.addEventListener('click',function(){
+menu.classList.toggle('active')
+});
